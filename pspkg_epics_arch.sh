681a312507a3fdebf8c9b834fc533c7dcf9e39f2
Bruce Hill <bhill@slac.stanford.edu>
2016-01-06 05:03:23 +0000

    Made pspkg_epics_arch.sh more robust
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/package/trunk/pkg_mgr/etc@23639 ececa60f-9f63-0410-97c7-ea05888eb260

--- pspkg_epics_arch.sh
+++ pspkg_epics_arch.sh
@@ -2,9 +2,13 @@
 #
 # To be run as a program, not sourced. Check exit status.
 #
-if [ ."$EPICS_BASE" == . ]; then
-    exit 1
+if [ ."$EPICS_BASE" != . ]; then
+	$EPICS_BASE/startup/EpicsHostArch.pl
+	exit 0
+fi
+if [ ."$EPICS_BASE_TOP" != . ]; then
+	$EPICS_BASE_TOP/current/startup/EpicsHostArch.pl
+	exit 0
 fi
 
-$EPICS_BASE/startup/EpicsHostArch.pl
-exit 0
+exit 1
