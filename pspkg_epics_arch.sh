1fd5adcda480aa19f9bb01e4e17fe41f19e15106
Abdullah Rashed Ahmed <apra@slac.stanford.edu>
2016-09-02 18:56:18 +0000

    Committing change to source by bruce
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/package/trunk/pkg_mgr/etc@26647 ececa60f-9f63-0410-97c7-ea05888eb260

--- pspkg_epics_arch.sh
+++ pspkg_epics_arch.sh
@@ -2,13 +2,25 @@
 #
 # To be run as a program, not sourced. Check exit status.
 #
-if [ ."$EPICS_BASE" != . ]; then
-	$EPICS_BASE/startup/EpicsHostArch.pl
-	exit 0
-fi
-if [ ."$EPICS_BASE_TOP" != . ]; then
-	$EPICS_BASE_TOP/current/startup/EpicsHostArch.pl
-	exit 0
+if [ "$(which perl)" != "" ]; then
+	STARTUP=unknown
+	if [    -d "$EPICS_BASE_TOP/current/startup" ]; then
+		STARTUP=$EPICS_BASE_TOP/current/startup/EpicsHostArch.pl
+	elif [  -d "$EPICS_BASE/startup" ]; then
+		STARTUP=$EPICS_BASE/startup
+	elif [  -d "/reg/g/pcds/package/epics/3.14/base/R3.14.12-0.4.0/startup" ]; then
+		STARTUP=/reg/g/pcds/package/epics/3.14/base/R3.14.12-0.4.0/startup
+	elif [  -d "/afs/slac/g/pcds/package/epics/3.14/base/R3.14.12-0.4.0/startup" ]; then
+		STARTUP=/afs/slac/g/pcds/package/epics/3.14/base/R3.14.12-0.4.0/startup
+	fi
+	if [ -f "$STARTUP/EpicsHostArch.pl" ]; then
+		perl -X $STARTUP/EpicsHostArch.pl
+		exit 0
+	fi
+else
+	# Default to linuxRT if perl isn't installed
+	echo linuxRT_glibc-x86_64
+	exit 1
 fi
 
 exit 1
