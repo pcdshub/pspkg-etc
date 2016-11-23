0c6ebd6c80ce8e7c0124ae1cc8318dbd26f01dce
Bruce Hill <bhill@slac.stanford.edu>
2016-11-23 04:33:54 +0000

    Reworked to not use perl and support LCLS too
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/package/trunk/pkg_mgr/etc@27314 ececa60f-9f63-0410-97c7-ea05888eb260

--- pspkg_epics_arch.sh
+++ pspkg_epics_arch.sh
@@ -2,25 +2,23 @@
 #
 # To be run as a program, not sourced. Check exit status.
 #
-if [ "$(which perl)" != "" ]; then
-	STARTUP=unknown
-	if [    -d "$EPICS_BASE_TOP/current/startup" ]; then
-		STARTUP=$EPICS_BASE_TOP/current/startup/EpicsHostArch.pl
-	elif [  -d "$EPICS_BASE/startup" ]; then
-		STARTUP=$EPICS_BASE/startup
-	elif [  -d "/reg/g/pcds/package/epics/3.14/base/R3.14.12-0.4.0/startup" ]; then
-		STARTUP=/reg/g/pcds/package/epics/3.14/base/R3.14.12-0.4.0/startup
-	elif [  -d "/afs/slac/g/pcds/package/epics/3.14/base/R3.14.12-0.4.0/startup" ]; then
-		STARTUP=/afs/slac/g/pcds/package/epics/3.14/base/R3.14.12-0.4.0/startup
-	fi
-	if [ -f "$STARTUP/EpicsHostArch.pl" ]; then
-		perl -X $STARTUP/EpicsHostArch.pl
-		exit 0
-	fi
-else
-	# Default to linuxRT if perl isn't installed
-	echo linuxRT_glibc-x86_64
-	exit 1
+STARTUP=unknown
+if [  -d "$EPICS_BASE/startup" ]; then
+	STARTUP=$EPICS_BASE/startup
+elif [  -d "$EPICS_BASE_RELEASE/startup" ]; then
+	STARTUP=$EPICS_BASE/startup
+elif [    -d "$EPICS_BASE_TOP/current/startup" ]; then
+	STARTUP=$EPICS_BASE_TOP/current/startup/EpicsHostArch.pl
+elif [  -d "/reg/g/pcds/package/epics/3.14/base/R3.14.12-0.4.0/startup" ]; then
+	STARTUP=/reg/g/pcds/package/epics/3.14/base/R3.14.12-0.4.0/startup
+elif [  -d "/afs/slac/g/lcls/epics/base/R3.14.12.4-1.2/startup" ]; then
+	STARTUP=/afs/slac/g/lcls/epics/base/R3.14.12.4-1.2/startup
+elif [  -d "/afs/slac/g/pcds/epics/base/R3.14.12-0.4.0/startup" ]; then
+	STARTUP=/afs/slac/g/pcds/epics/base/R3.14.12-0.4.0/startup
+fi
+if [ -x "$STARTUP/EpicsHostArch" ]; then
+	$STARTUP/EpicsHostArch
+	exit 0
 fi
 
 exit 1
