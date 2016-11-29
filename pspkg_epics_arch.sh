2a69d04c90c91cb7a52c2fc000e6c2fa6c6f5195
Bruce Hill <bhill@slac.stanford.edu>
2016-11-29 05:07:26 +0000

    Fixed errors in setting STARTUP from EPICS_BASE_RELEASE
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/package/trunk/pkg_mgr/etc@27360 ececa60f-9f63-0410-97c7-ea05888eb260

--- pspkg_epics_arch.sh
+++ pspkg_epics_arch.sh
@@ -3,18 +3,18 @@
 # To be run as a program, not sourced. Check exit status.
 #
 STARTUP=unknown
-if [  -d "$EPICS_BASE/startup" ]; then
-	STARTUP=$EPICS_BASE/startup
+if [    -d "$EPICS_BASE/startup" ]; then
+    STARTUP=$EPICS_BASE/startup
 elif [  -d "$EPICS_BASE_RELEASE/startup" ]; then
-	STARTUP=$EPICS_BASE/startup
-elif [    -d "$EPICS_BASE_TOP/current/startup" ]; then
-	STARTUP=$EPICS_BASE_TOP/current/startup/EpicsHostArch.pl
+    STARTUP=$EPICS_BASE_RELEASE/startup
+elif [  -d "$EPICS_BASE_TOP/current/startup" ]; then
+    STARTUP=$EPICS_BASE_TOP/current/startup
 elif [  -d "/reg/g/pcds/package/epics/3.14/base/R3.14.12-0.4.0/startup" ]; then
-	STARTUP=/reg/g/pcds/package/epics/3.14/base/R3.14.12-0.4.0/startup
+    STARTUP=/reg/g/pcds/package/epics/3.14/base/R3.14.12-0.4.0/startup
 elif [  -d "/afs/slac/g/lcls/epics/base/R3.14.12.4-1.2/startup" ]; then
-	STARTUP=/afs/slac/g/lcls/epics/base/R3.14.12.4-1.2/startup
+    STARTUP=/afs/slac/g/lcls/epics/base/R3.14.12.4-1.2/startup
 elif [  -d "/afs/slac/g/pcds/epics/base/R3.14.12-0.4.0/startup" ]; then
-	STARTUP=/afs/slac/g/pcds/epics/base/R3.14.12-0.4.0/startup
+    STARTUP=/afs/slac/g/pcds/epics/base/R3.14.12-0.4.0/startup
 fi
 if [ -x "$STARTUP/EpicsHostArch" ]; then
 	$STARTUP/EpicsHostArch
